e275ecb43de890ff2303478e97eda8618558db6a
Raymond Nazaryan <rayn2427@gmail.com>
2017-10-16 14:06:42 -0700

    AXONRadio web application
    Initial commit

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span
+				strong AXON
+				|Radio
+</template>
+
+<script>
+
+	export default {
+
+	};
+
+</script>
+
+<style lang="scss">
+</style>
