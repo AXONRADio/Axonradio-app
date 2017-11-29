cb9d5377ec538e98b0cb9087153766f1faef22f5
henryq95 <quinteros7766@gmail.com>
2017-11-28 22:16:29 -0800

    YouTube Embed partially working, videos not playing. Added vue-youtube-embed.js to \client\app\core\
    updated home index.vue file

--- client/app/modules/home/index.vue
+++ client/app/modules/home/index.vue
@@ -55,13 +55,15 @@
 					.card
 						.block
 							.title youtube
-									div(id='#app1')
+									div(id='app1')
 									section
 											h2 listening events
-											youtube(:video-id='aFTo73u2wZ8', @ready='ready', @playing='playing')
+											|
+											youtube(:video-id='videoId', @ready='ready', @playing='playing')
 									section
 											h2 add options
-											youtube(:video-id='aFTo73u2wZ8', player-width='1280', player-height='750', :player-vars='{autoplay: 1}')
+											|
+											youtube(:video-id='videoId', player-width='1280', player-height='750', :player-vars='{autoplay: 1}')
 
 
 
@@ -71,13 +73,12 @@
 'use strict'
 import Vue from 'vue'
 import VueYouTubeEmbed from 'vue-youtube-embed'
-
 Vue.use(VueYouTubeEmbed)
 
 const app = new Vue({
   el: '#app1',
   data: {
-    videoId: 'videoId',
+    videoId: '3M_5oYU-IsU',
   },
   methods: {
     ready (player) {
@@ -103,161 +104,4 @@ const app = new Vue({
 })
 </script>
 
-<script>
-	export default {
 
-		methods: {
-			getTypographyInfo(elType) {
-				if (this.$el) {
-					let element = this.$el.querySelector(elType);
-					if (element) {
-						let style = window.getComputedStyle(element, null);
-						return style.fontFamily.split(",")[0] + " " + style.fontWeight + ", " + style.fontSize;
-					}
-				}
-			}
-		}
-	};
-
-</script>
-
-
-
-<style lang="scss" scoped>
-	@import "../../../scss/themes/blurred/variables";
-
-	.container {
-		padding: 0 1rem;
-	}
-
-	section {
-		&:not(:last-child) {
-			margin-bottom: 40px;
-		}
-
-		> h2 {
-			font-size: 1.5rem;
-			margin-bottom: 20px;
-			padding: 8px 10px;
-
-			background-color: rgba(darken($backgroundColor, 10%), 0.6);
-			border: 1px solid darken($backgroundColor, 8%);
-			border-radius: 8px;
-
-			.number {
-				color: #888;
-			}
-
-			.text {
-				margin-left: 4px;
-				font-weight: $fontLight;
-				text-transform: uppercase;
-			}
-
-			clear: both;
-
-		} // .title
-
-	} //. section
-
-	.buttons {
-		margin-bottom: 20px;
-	}
-
-	.list {
-		> * {
-			margin-bottom: 20px;
-		}
-	}
-
-	.panels {
-		align-items: flex-start;
-		.panel, .card {
-			margin: 20px;
-		}
-
-		.card {
-			max-width: 350px;
-		}
-	}
-
-	.colors {
-		$boxSize: 150px;
-
-		.box {
-			width: $boxSize;
-			height: $boxSize + 20px;
-
-			border: 1px solid darken($backgroundColor, 10%);
-			border-radius: 6px;
-
-			margin: 5px 20px;
-			padding: 2px;
-
-			.caption {
-				float: left;
-				width: 100%;
-				text-align: center;
-			}
-
-			.main {
-				float: left;
-				width: 100%;
-				height: $boxSize - 60px;
-			} // .main
-
-			.light {
-				float: left;
-				width: 50%;
-				height: 30px;
-
-			} // .light
-
-			.dark {
-				float: right;
-				width: 50%;
-				height: 30px;
-
-			} // .dark
-
-			.code {
-				float: left;
-				width: 100%;
-				position: relative;
-				margin-top: 5px;
-
-				&:after {
-					position: absolute;
-					top: 0; bottom: 0;
-					left: 0; right: 0;
-					margin: auto;
-					width: 100%;
-					text-align: center;
-					font-family: "Consolas";
-					color: White;
-				}
-			} // .code
-
-			$colors: $color1, $color2, $color3, $color4, $color5;
-
-			$colors-light: $color1-light, $color2-light, $color3-light, $color4-light, $color5-light;
-			$colors-dark: $color1-dark, $color2-dark, $color3-dark, $color4-dark, $color5-dark;
-
-			@for $i from 1 through 5 {
-
-				$c: nth($colors, $i);
-
-				&.box#{$i} {
-					.main { background-color: $c; }
-					.light { background-color: nth($colors-light, $i); }
-					.dark { background-color: nth($colors-dark, $i); }
-					.code:after {	content: "" + $c; }
-
-				} // box
-
-			} // for
-
-		} // .box
-
-	} // .colors
-</style>
